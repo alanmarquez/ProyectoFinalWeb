b44741e049a939569f4fc5c724da60eb4a77b28a
Yiaen <yael.96@hotmail.com>
2017-05-30 12:32:29 -0600

    Delete conect.php

--- conn/conect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Datos de la base de datos.
-    $host="localhost";
-    $user="root";
-    $pass="123456";	
-    $db="proyecto_final";
-
-    $connect = new mysqli($host,$user,$pass,$db) or die ("error" . mysqli_errno($connect));
-?>
